d066c73ed2485e1b28b764c2ebe5c66e80e4adb4
avishkar <ahbagade2000@gmail.com>
2024-12-09 20:52:20 +0530

    cors options

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
-from routes import api_routes
+from routes import *
 
 app = Flask(__name__)
 CORS(app)
+cors_option = {
+    "methods": ['GET','POST','DELETE', 'PUT', 'PATCH', 'OPTIONS'],
+    "origins": ['http://localhost:4200', "127.0.0.1:4200"]
+}
 
 app.register_blueprint(api_routes)
 
